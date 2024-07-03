d1801c6c34526fe0a0b5bc2e2b20b5bb8abca426
Anna <cameaann@gmail.com>
2024-07-04 00:38:33 +0300

    deleting comments

--- src/services/wikiService.js
+++ src/services/wikiService.js
@@ -1,15 +1,10 @@
 import axios from "axios";
 
-const getArticles = async (title, lang) => {
-console.log(lang);
+const getArticles = async (title) => {
 const url = "https://en.wikipedia.org/api/rest_v1/page";
 
   try {
-//     console.log(title);
     const res = await axios.get(`${url}/summary/${encodeURIComponent(title)}`, {
-//       headers: {
-//         "User-Agent": "wikiSearchApp/1.0 (cameaann@gmail.com)",
-//       },
     });
 //     console.log(res.data);
     return res.data;
