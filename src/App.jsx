8448c018a9fc36992386d0c5a7c66e10ff337873
Anna <cameaann@gmail.com>
2024-07-04 00:39:07 +0300

    adding Search and SearchResult

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
+import Search from "./components/Search";
+import wikiService from "./services/wikiService";
+import SearchResult from "./components/SearchResult";
 
 const App = () => {
+  const [searchW, setSearchW] = useState("book");
+  const [result, setResult] = useState();
+
+  useEffect(() => {
+    try {
+
+      wikiService.getArticles(searchW).then((res) => {
+        console.log(res);
+        setResult(res);
+      });
+    } catch (error) {
+      return console.error();
+    }
+  }, [searchW]);
+
+  
+  const handleOnChange = (searchWord) => {
+    setSearchW(searchWord);
+  };
+
+  const { title, description, content_urls } = { ...result };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
       <h1>Wiki</h1>
-
+      <Search handleChange={handleOnChange} />
+      <SearchResult
+        title={title}
+        description={description}
+        articleLink={content_urls}
+      />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
