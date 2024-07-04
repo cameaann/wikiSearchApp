7d4191b776615937a2244e7acdb1d63601cef9a8
Anna <cameaann@gmail.com>
2024-07-04 16:54:51 +0300

    fixed description in AllResults

--- src/components/AllResults.jsx
+++ src/components/AllResults.jsx
@@ -1,16 +1,16 @@
 import Result from "./Result";
 
-const AllResults = ({ results }) => {
+const AllResults = ({ results = []}) => {
   console.log(results);
   return (
     <section>
-      { results && results.length > 0 ? (
+      { (results.length > 0) ? (
         results.map((res, index ) => (
         <Result 
           key = {index}
           title={res.title}
-          description={res.description.lenth>0 ? res.description : res.extract}
-          link={res.desktop}
+          description={res.description ? res.description : res.extract}
+          link={res.content_urls}
         />
       ))
       ) : ( <p>No results found.</p>)
