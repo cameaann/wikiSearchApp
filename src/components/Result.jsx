2bfadc94a2252a3618bbe8c813c2bf5d6c63ce1a
Anna <cameaann@gmail.com>
2024-07-04 16:33:55 +0300

    changed Result component

---
+++ src/components/Result.jsx
@@ -0,0 +1,13 @@
+const Result = ({title, description, articleLink}) => {
+
+    const { desktop } = {...articleLink}
+    return(
+        <section>
+            <h2>{title}</h2>
+            <p>{description}</p>
+            {/* <a href={desktop.page != undefined ? desktop.page : ''} target='blank'>See the link</a> */}
+        </section>
+    )
+}
+
+export default Result
