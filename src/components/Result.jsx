0ff229572a633ed228409847cc412e9eb3d99ac5
Anna <cameaann@gmail.com>
2024-07-04 16:55:40 +0300

    adding the link to Result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,11 +1,11 @@
-const Result = ({title, description, articleLink}) => {
+const Result = ({title, description, link}) => {
 
-    const { desktop } = {...articleLink}
+    const { desktop } = {...link}
     return(
         <section>
             <h2>{title}</h2>
             <p>{description}</p>
-            {/* <a href={desktop.page != undefined ? desktop.page : ''} target='blank'>See the link</a> */}
+            <a href={desktop.page != undefined ? desktop.page : ''} target='blank'>See the link</a>
         </section>
     )
 }
