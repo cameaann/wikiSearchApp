b6a6109f48f7d0278ae32697e8d68f7572e4b4e0
Anna <cameaann@gmail.com>
2024-07-05 12:40:24 +0300

    adding SwitchModes

---
+++ src/components/SwitchModes.jsx
@@ -0,0 +1,11 @@
+const SwitchModes = () =>{
+
+    const [theme, handleClick] = useTheme('light');
+    return(
+        <button className="switchBtn" onClick = {handleClick}>
+            <span className="btn-word">{theme === 'light' ? 'Dark':'Light'}</span>
+        </button>
+    )
+}
+
+export default SwitchModes;
