19f04447e1b003ea2292da77e7c08de36c93e73c
Anna <cameaann@gmail.com>
2024-07-05 12:41:43 +0300

    adding condition for search word

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,38 @@
-import "./App.css";
 import { useEffect, useState } from "react";
 import Search from "./components/Search";
 import wikiService from "./services/wikiService";
 import AllResults from "./components/AllResults";
+import { DarkModeProvider } from "./DarkModeContext";
 
 const App = () => {
-  const [searchW, setSearchW] = useState("book");
+  const [searchW, setSearchW] = useState("");
   const [results, setResult] = useState();
 
   useEffect(() => {
     try {
-
-      wikiService.getArticles(searchW).then((res) => {
-        console.log(res);
-        setResult(res);
-      });
+      if (searchW.length > 0) {
+        wikiService.getArticles(searchW).then((res) => {
+          console.log(res);
+          setResult(res);
+        });
+      }
     } catch (error) {
       return console.error();
     }
   }, [searchW]);
 
-  
   const handleOnChange = (searchWord) => {
     setSearchW(searchWord.toLowerCase());
   };
 
-
   return (
-    <>
-      <h1>Wiki</h1>
-      <Search handleChange={handleOnChange} />
-      <AllResults results = {results}/>
-    </>
+    <DarkModeProvider>
+      {/* <main> */}
+        <h1>Wiki</h1>
+        <Search handleChange={handleOnChange} />
+        {searchW.length > 0 ? <AllResults results={results} /> : <div></div>}
+      {/* </main> */}
+    </DarkModeProvider>
   );
 };
 
